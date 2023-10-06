05f85f0e8434b581447dcb12a90b15ea26f4dda6
gosha <99@.ru>
2023-10-06 22:33:45 +0500

    js-5

---
+++ app.js
@@ -0,0 +1,30 @@
+// Первая задача
+
+// const users = {};
+
+// for (let i = 1; i <= 3; i++) {
+//     let name = prompt('Введите свое имя')
+//     let age = +prompt('Введите свой возраст')
+
+//     users[i] = {
+//         name,
+//         age
+//     }
+//     console.log(`Пользователь ${i}`);
+//     console.log(`Ваше имя ${users[i].name}`);
+//     console.log(`Ваш возраст ${users[i].age}`);
+// }
+
+// Вторая задача
+
+// let rec = receipt()
+// let menu = 'Вы заказали'
+// let delivery = 9000
+
+// for (let key in cart) {
+//     menu += ${key}
+//     menu += ${cart[key].info}
+//     delivery += cart[key].price
+// }
+
+// console.log(`${menu} | Общая стоимость ${delivery} сум с доставкой (9000)`);
